e96729a361bfba2570061fa8e38cc453f4e5a5f4
rpsaavedra@uc.cl <rpsaavedra@uc.c>
2016-05-13 13:55:11 -0300

    intento render array

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,10 @@ class ApiController < ApplicationController
   	tag= "melipillazo"
   	token="2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402"
   	cantidad = getCantidad(tag,token)
-  	response = { :metadata =>  { :cantidad => cantidad}}
+  	datos= getPost(tag,token)
+  	persona = getPostx(datos,0)
+
+  	response = { :metadata =>  { :cantidad => cantidad}, :posts => [ persona, persona ] }
   	render :json => response
 
   end
@@ -23,6 +26,32 @@ class ApiController < ApplicationController
  def getCantidad(tag,token)
  	totales =JSON.parse(HTTP.headers(:"Content-Type" => "application/json").get("https://api.instagram.com/v1/tags/"+ tag+"?access_token="+token).to_s, :symbolize_names => true)
  	return totales[:data][:media_count]
+ end
+
+
+ def getPost(tag, token)
+ 	post =JSON.parse(HTTP.headers(:"Content-Type" => "application/json").get("https://api.instagram.com/v1/tags/"+ tag+ "/media/recent?access_token="+ token).to_s, :symbolize_names => true)
+ 	return post[:data]
+ end
+
+
+ def getPostx(data,numero)
+ 	tags = data[numero][:tags]
+ 	username = data[numero][:caption][:from][:username]
+ 	likes =data[numero][:likes][:count]
+ 	type = data[numero][:type]
+ 	if(type == "image")
+ 		url = data[numero][:images][:standard_resolution][:url]
+
+ 	end
+
+ 	if(type == "video")
+ 		url = data[numero][:videos][:standard_resolution][:url]
+ 	end
+ 	caption = data[numero][:caption][:text]
+
+ 	return  {:tags =>tags , :username =>username, :likes => likes, :type=>type, :url => url, :caption => caption }
+ 	 
  end
 
 end
