eac0466a9e5a399ce3d06a0eaeaeb25ccecedda9
rpsaavedra@uc.cl <rpsaavedra@uc.c>
2016-05-13 16:13:51 -0300

    try 3 de post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'api/index'
-
+  get 'instagram/tag/buscar'  => 'api#index'
+  
   post 'instagram/tag/buscar' => 'api#buscar'
+
+  match '/instagram/tag/buscar' => 'api#buscar', via: :post
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
